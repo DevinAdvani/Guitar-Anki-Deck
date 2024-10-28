f99798c2ef69055af40246a8e2c3ae9136f5ca89
DevinAdvani <advani.devin@gmail.com>
2024-10-28 17:59:25 +0000

    Made folder to sort in

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import genanki
+import os
 
-tunings = "EADGBE"
+tunings = "EADGBE"#"DADGAD"
 notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
 start_fret = -1
-max_fret = 2#12
+max_fret = 1
 chord_shapes = [[0,4,7], [0,4,7,11], [0,4,7,9], [0,4,7,9,14], [0,4,7,9,14], [0,4,7,9,14,17], [0,3,7],[0,3,7,10], [0,3,7,9],[0,3,7,9,14],[0,3,7,10,14], [0,3,7,10,14,17], [0,3,7,11], [0,3,7,11,14], [0,4,7,10], [0,4,7,10,14], [0,4,7,10,14,17], [0,3,6], [0,3,6,9], [0,3,6,10],[0,4,8],[0,4,8,10], [0,4,6,10], [0,4,8,10], [0,4,7,10,13], [0,4,7,10,15], [0,4,7,10,18], [0,7],[0,4,7,14], [0,2,4,7],[0,4,7,17],[0,4,5,7], [0,5,7], [0,2,7]]
-overall_chord_names = ["Major", "Major 7", "6", "6/9", "Major 9", "Major 11", "Minor", "Minor 7", "Minor 6", "Minor 6/9", "Minor 9", "Minor 11", "Minor Major 7", "Minor Major 9", "7", "9", "11", "Diminished", "Diminished 7", "Half-Diminished", "Augmented", "Augmented 7", "7-5", "7+5", "7-9", "7+9", "7#11", "5", "Add 9", "Add 2", "Add 11", "Add 4", "Suspended 4", "Suspended 2"]
-chord_notes = []
+overall_chord_names = ["Major", "Major 7", "6", "69", "Major 9", "Major 11", "Minor", "Minor 7", "Minor 6", "Minor 69", "Minor 9", "Minor 11", "Minor Major 7", "Minor Major 9", "7", "9", "11", "Diminished", "Diminished 7", "Half-Diminished", "Augmented", "Augmented 7", "7-5", "7+5", "7-9", "7+9", "7#11", "5", "Add 9", "Add 2", "Add 11", "Add 4", "Suspended 4", "Suspended 2"]
+chord_notes = [] # fix slashes
 chord_names = []
+
+for i in overall_chord_names:
+    try:
+        os.rmdir(tunings + "/" + i)
+    except:
+        pass
+
+try:
+    os.rmdir(tunings)
+except:
+    pass
+
+os.mkdir(tunings)
+
+for i in overall_chord_names:
+    try:
+        os.mkdir(tunings + "/" + i)
+    except:
+        pass
+
+def pitch_up_a_tab(input_tab):
+    output_tab = []
+    for i in range(0,6):
+        try:
+            output_tab.append(input_tab[i]+1)
+        except:
+            output_tab.append("X")
+    return output_tab
+
+#print(pitch_up_a_tab([1,"X",3,4,5,"X"]))
 
 def shift(input_note, semi_tones):
     position = notes.index(input_note)
@@ -31,20 +62,20 @@ def chord_to_chromatic_notes(input_chord_list):
         output.append(set(chord))
     return output
 
-def produce_tabs(input_start, input_max_fret):
+def produce_open_chord_tabs():
     tabs = []
-    for a in range(input_start, input_max_fret+1):
-        for b in range(input_start, input_max_fret+1):
-            for c in range(input_start, input_max_fret+1):
-                for d in range(input_start, input_max_fret+1):
-                    for e in range(input_start, input_max_fret+1):
-                        for f in range(input_start, input_max_fret+1):
+    for a in range(-1,max_fret+1):
+        for b in range(-1,max_fret+1):
+            for c in range(-1,max_fret+1):
+                for d in range(-1,max_fret+1):
+                    for e in range(-1,max_fret+1):
+                        for f in range(-1,max_fret+1):
                             tab = [a,b,c,d,e,f]
-                            for i in range(0,6):
-                                if (tab[i] == -1):
-                                    tab[i] = "X"
-                            tabs.append(tab)
-    tabs.remove(["X","X","X","X","X","X"])
+                            if 0 in tab:
+                                for i in range(0,6):
+                                    if (tab[i] == -1):
+                                        tab[i] = "X"
+                                tabs.append(tab)
     return tabs
 
 for i in range(0,len(chord_shapes)):
@@ -53,7 +84,18 @@ for i in range(0,len(chord_shapes)):
         chord_names.append(notes[j] + " " + overall_chord_names[i])
         chord_notes.append(tmp_notes[j])
 
+open_chords = produce_open_chord_tabs()
 
+chords = []
+
+for i in range(0,len(open_chords)):
+    try:
+        chords.append([chord_names[chord_notes.index(tab_to_note(open_chords[i]))], open_chords[i]])
+    except:
+        pass
+
+
+"""
 def make_deck():
     output_tabs = produce_tabs(start_fret, max_fret)
 
@@ -89,4 +131,4 @@ def make_deck():
         
     genanki.Package(my_deck).write_to_file(tunings + " until " + str(max_fret) + '.apkg')
 
-make_deck()
+"""
